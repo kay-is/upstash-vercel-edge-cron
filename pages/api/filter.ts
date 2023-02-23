672cd8f241230c266dbe6e9c340b2072c38397b1
K <kay-is@users.noreply.github.com>
2023-02-23 14:08:39 +0000

    Update route

--- pages/api/filter.ts
+++ pages/api/filter.ts
@@ -4,6 +4,7 @@ import { filter } from "@/utils/word-filter"
 export const config = { runtime: "edge" }
 
 export default async function handler(request: NextApiRequest) {
+  console.log(request.body)
   const maskedText = await filter(request.body.text)
 
   return new Response(JSON.stringify({ text: maskedText }), {
