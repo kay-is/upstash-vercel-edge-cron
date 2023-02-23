5fa35b211326e84528aa180d6e845e78d9848b91
K <kay-is@users.noreply.github.com>
2023-02-23 14:25:37 +0000

    UPDATE

--- pages/api/filter.ts
+++ pages/api/filter.ts
@@ -4,8 +4,9 @@ import { filter } from "@/utils/word-filter"
 export const config = { runtime: "edge" }
 
 export default async function handler(request: NextApiRequest) {
-  console.log(request.body)
-  const maskedText = await filter(request.body.text)
+  const { text } = await new Response(request.body).json()
+  console.log(text)
+  const maskedText = await filter(text)
   return new Response(JSON.stringify({ text: maskedText }), {
     status: 200,
     headers: { "content-type": "application/json" },
