e33ad3aae760ca046625ae0d0556e276c5394359
K <kay-is@users.noreply.github.com>
2023-02-23 14:12:02 +0000

    UPDATE

--- pages/api/filter.ts
+++ pages/api/filter.ts
@@ -6,7 +6,6 @@ export const config = { runtime: "edge" }
 export default async function handler(request: NextApiRequest) {
   console.log(request.body)
   const maskedText = await filter(request.body.text)
-
   return new Response(JSON.stringify({ text: maskedText }), {
     status: 200,
     headers: { "content-type": "application/json" },
